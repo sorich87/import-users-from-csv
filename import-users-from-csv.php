c16aab50ea84a1aa14e6f8e2f1209768b9eeea69
Ulrich SOSSOU <ulrich.sossou@intside.com>
2011-05-21 11:51:46 +0000

    Made the plugin generic so that it can be used on any website

--- import-users-from-csv.php
+++ import-users-from-csv.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * @package externals links
- * @version 0.1
+ * @package Import_Users_from_CSV
+ * @version 0.2
  */
 /*
 Plugin Name: Import Users from CSV
 Plugin URI: http://intside.com/
 Description: Import Users data and metadata from csv file.
 Author: Intside
-Version: 0.1
+Version: 0.2
 Author URI: http://intside.com/
 */
 
@@ -28,13 +28,12 @@ if ( ! function_exists( 'str_getcsv' ) ) {
 	}
 }
 
-
 /**
  * Main plugin class
  *
  * @since 0.1
  **/
-class IS_SM_Import_Users {
+class IS_IU_Import_Users {
 
 	/**
 	 * Class contructor
@@ -52,7 +51,7 @@ class IS_SM_Import_Users {
 	 * @since 0.1
 	 **/
 	public function add_admin_pages() {
-		add_users_page( __( 'Import From CSV' ), __( 'Import From CSV' ), 'manage_options', 'import-users-from-csv', array( $this, 'users_page' ) );
+		add_users_page( __( 'Import From CSV' ), __( 'Import From CSV' ), 'create_users', 'import-users-from-csv', array( $this, 'users_page' ) );
 	}
 
 	/**
@@ -61,78 +60,92 @@ class IS_SM_Import_Users {
 	 * @since 0.1
 	 **/
 	public function process_csv() {
-		if ( isset( $_FILES['users_csv']['tmp_name'] ) ) {
-			$rows            = file( $_FILES['users_csv']['tmp_name'] );
-			$headers         = str_getcsv( $rows[0] );
-			$rows            = array_slice( $rows, 1 );
-			$errors          = array();
-			$userdata_fields = array(
-				'ID', 'user_login', 'user_pass',
-				'user_email', 'user_url', 'user_nicename',
-				'display_name', 'user_registered', 'first_name',
-				'last_name', 'nickname', 'description',
-				'rich_editing', 'comment_shortcuts', 'admin_color',
-				'use_ssl', 'show_admin_bar_front', 'show_admin_bar_admin'
-			);
+		if ( isset( $_POST['_wpnonce-is-iu-import-users-users-page_import'] ) ) {
+			check_admin_referer( 'is-iu-import-users-users-page_import', '_wpnonce-is-iu-import-users-users-page_import' );
 
-			$sms = new IS_SM_Sms_Messages();
+			if ( isset( $_FILES['users_csv']['tmp_name'] ) ) {
+				ini_set( 'auto_detect_line_endings', true );
 
-			foreach ( $rows as $rkey => $row ) {
-				$columns = str_getcsv( $row );
-				$userdata = $usermeta = array();
+				$rows = file( $_FILES['users_csv']['tmp_name'] );
+				if ( ! $rows )
+					wp_redirect( add_query_arg( 'import', 'data', wp_get_referer() ) );
 
-				foreach ( $columns as $ckey => $column ) {
-					$column_name = $headers[$ckey];
-					$column = trim( $column );
+				$password_nag 		= isset( $_GET['password_nag'] ) ? $_GET['password_nag'] : false;
+				$notification_email = isset( $_GET['notification_email'] ) ? $_GET['notification_email'] : false;
+				$errors = $user_ids = array();
+				$headers            = str_getcsv( $rows[0] );
+				$rows               = array_slice( $rows, 1 );
+				$userdata_fields    = array(
+					'ID', 'user_login', 'user_pass',
+					'user_email', 'user_url', 'user_nicename',
+					'display_name', 'user_registered', 'first_name',
+					'last_name', 'nickname', 'description',
+					'rich_editing', 'comment_shortcuts', 'admin_color',
+					'use_ssl', 'show_admin_bar_front', 'show_admin_bar_admin',
+					'role'
+				);
 
-					if ( in_array( $column_name, $userdata_fields ) ) {
-						$userdata[$column_name] = $column;
-					} elseif ( in_array( $column_name, array( 'e', 'e_pl', 'r', 'c', 'a_m' ) ) ) {
-						if ( ! empty( $column ) )
-							$usermeta['profession'] = $column;
+				do_action( 'is_iu_pre_users_import', $headers, $rows );
+
+				foreach ( $rows as $rkey => $row ) {
+					$columns = str_getcsv( $row );
+					$userdata = $usermeta = array();
+
+					foreach ( $columns as $ckey => $column ) {
+						$column_name = $headers[$ckey];
+						$column = trim( $column );
+
+						if ( in_array( $column_name, $userdata_fields ) ) {
+							$userdata[$column_name] = $column;
+						} else {
+							$usermeta[$column_name] = $column;
+						}
+					}
+
+					$userdata = apply_filters( 'is_iu_import_userdata', $userdata, $usermeta );
+					$usermeta = apply_filters( 'is_iu_import_usermeta', $usermeta, $userdata );
+
+					if ( empty( $userdata['user_pass'] ) )
+						$userdata['user_pass'] = wp_generate_password( 12, false );
+
+					if ( empty( $userdata['ID'] ) )
+						$user_id = wp_insert_user( $userdata );
+					else
+						$user_id = wp_update_user( $userdata );
+
+					if ( is_wp_error( $user_id ) ) {
+						$errors[$rkey] = $user_id;
 					} else {
-						$usermeta[$column_name] = $column;
+						foreach ( $usermeta as $metakey => $metavalue ) {
+							update_user_meta( $user_id, $metakey, $metavalue );
+						}
+
+						if ( empty( $userdata['ID'] ) ) {
+							if ( $password_nag )
+								update_user_option( $user_id, 'default_password_nag', true, true );
+
+							if ( $notification_email )
+								wp_new_user_notification( $user_id, $user_pass );
+						}
+
+						$user_ids[] = $user_id;
 					}
 				}
 
-				if ( ! empty( $userdata['first_name'] ) ) {
-					$userdata['first_name'] = strtolower( $userdata['first_name'] );
-					$userdata['first_name'] = ucfirst( $userdata['first_name'] );
-				}
+				do_action( 'is_iu_post_users_import', $user_ids, $errors );
 
-				if ( ! empty( $usermeta['phone'] ) ) {
-					$usermeta['phone'] = str_replace( ' ', '', $usermeta['phone'] );
-					$usermeta['phone'] = '229' . $usermeta['phone'];
-				}
+				$this->log_errors( $errors );
 
-				if ( empty( $userdata['user_login'] ) )
-					$userdata['user_login'] = $usermeta['phone'];
-
-				if ( empty( $userdata['user_email'] ) )
-					$userdata['user_email'] = 'test+' . $userdata['user_login'] . '@localhost.localdomain';
-
-				if ( empty( $userdata['user_pass'] ) )
-					$userdata['user_pass'] = wp_generate_password( 12, false );
-
-				if ( 11 != strlen( $usermeta['phone'] ) )
-					$userdata = array();
-
-				$user_id = wp_insert_user( $userdata );
-
-				if ( is_wp_error( $user_id ) ) {
-					$errors[$rkey] = $user_id;
+				if ( $errors && ! $user_ids ) {
+					wp_redirect( add_query_arg( 'import', 'fail', wp_get_referer() ) );
+				} elseif ( $errors ) {
+					wp_redirect( add_query_arg( 'import', 'errors', wp_get_referer() ) );
 				} else {
-					foreach ( $usermeta as $metakey => $metavalue ) {
-						update_user_meta( $user_id, $metakey, $metavalue );
-					}
-
-					$sms->send_registration_sms( $user_id );
+					wp_redirect( add_query_arg( 'import', 'success', wp_get_referer() ) );
 				}
+			} else {
+				wp_redirect( add_query_arg( 'import', 'file', wp_get_referer() ) );
 			}
-
-			$sms->process();
-
-			wp_redirect( add_query_arg( 'success', 'true', wp_get_referer() ) );
 		}
 	}
 
@@ -148,19 +161,93 @@ class IS_SM_Import_Users {
 
 <div class="wrap">
 	<h2><?php _e( 'Import users from a CSV file' ); ?></h2>
+	<?php
+	$error_log_file = plugin_dir_url( __FILE__ ) . 'errors.log';
+
+	if ( ! file_exists( $error_log_file ) )
+		@fopen( $error_log_file, 'x' );
+
+	if ( isset( $_GET['import'] ) ) {
+		if ( file_exists( $error_log_file ) )
+			$error_log_msg = ", please <a href='$error_log'>check error log</a>";
+
+		switch ( $_GET['import'] ) {
+			case 'file':
+				echo '<div class="error"><p><strong>Error during file upload.</strong></p></div>';
+				break;
+			case 'data':
+				echo '<div class="error"><p><strong>Cannot extract data from uploaded file or no file was uploaded.</strong></p></div>';
+				break;
+			case 'fail':
+				echo "<div class='error'><p><strong>No user was successfully imported$error_log_msg.</strong></p></div>";
+				break;
+			case 'errors':
+				echo "<div class='error'><p><strong>Some users were successfully imported but some were not$error_log_msg.</strong></p></div>";
+				break;
+			case 'success':
+				echo '<div class="updated"><p><strong>Users import was successful.</strong></p></div>';
+				break;
+			default:
+				break;
+		}
+	}
+	?>
 	<form method="post" action="" enctype="multipart/form-data">
+		<?php wp_nonce_field( 'is-iu-import-users-users-page_import', '_wpnonce-is-iu-import-users-users-page_import' ); ?>
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row"><?php _e( 'CSV file' ); ?></th>
-				<td><input type="file" name="users_csv" value="" class="all-options" /></td>
+				<th scope="row"><label for"users_csv"><?php _e( 'CSV file' ); ?></label></th>
+				<td><input type="file" id="users_csv" name="users_csv" value="" class="all-options" /></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><?php _e( 'Notification email' ); ?></th>
+				<td><fieldset>
+					<legend class="screen-reader-text"><span><?php _e( 'Notification email' ); ?></span></legend>
+					<label for="notification_email">
+						<input id="notification_email" name="notification_email" type="checkbox" value="1" />
+						Send to new users
+					</label>
+				</fieldset></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><?php _e( 'Password nag' ); ?></th>
+				<td><fieldset>
+					<legend class="screen-reader-text"><span><?php _e( 'Password nag' ); ?></span></legend>
+					<label for="password_nag">
+						<input id="password_nag" name="password_nag" type="checkbox" value="1" />
+						Show password nag on new users signon
+					</label>
+				</fieldset></td>
 			</tr>
 		</table>
 		<p class="submit">
-			<input type="submit" class="button-primary" value="<?php _e( 'Import Users' ); ?>" />
+			<input type="submit" class="button-primary" value="<?php _e( 'Upload' ); ?>" />
 		</p>
 	</form>
 <?php
+		if ( ! file_exists( $error_log_file ) )
+			echo '<p>Notice: please make the plugin directory writable so that you can see the error log.</p>';
+	}
+
+	/**
+	 * Log errors to a file
+	 *
+	 * @since 0.2
+	 **/
+	public function log_errors( $errors ) {
+		if ( empty( $errors ) )
+			return;
+
+		$log = @fopen( plugin_dir_path( __FILE__ ) . 'errors.log', 'a' );
+		@fwrite( $log, 'BEGIN ' . date( 'YmdHis', time() ) . "\n" );
+
+		foreach ( $errors as $key => $error ) {
+			$line = $key + 1;
+			@fwrite( $log, "[Line $line] $error\n" );
+		}
+
+		@fclose( $log );
 	}
 }
 
-new IS_SM_Import_Users;
+new IS_IU_Import_Users;
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plugin Name ===
+Contributors: sorich87
+Tags: user, users, csv, import, admin
+Requires at least: 3.1
+Tested up to: 3.2
+Stable tag: 0.1
+
+Import users from a CSV file
+
+== Description ==
+
+This plugin allows you to import users from an uploaded CSV file. It will add users with basic information as well as meta fields and user role.
+
+You can also send a notification to the user email.
+
+== Installation ==
+
+For an automatic installation through WordPress:
+
+1. Go to the 'Add New' plugins screen in your WordPress admin area
+1. Search for 'Import Users from CSV'
+1. Click 'Install Now' and activate the plugin
+1. Upload your CSV file in the 'Users' menu, under 'Import From CSV'
+
+Or use a nifty tool by WordPress lead developer Mark Jaquith:
+
+1. Visit [this link](http://coveredwebservices.com/wp-plugin-install/?plugin=import-users-from-csv) and follow the instructions.
+
+For a manual installation via FTP:
+
+1. Upload the `import-users-from-csv` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' screen in your WordPress admin area
+
+To upload the plugin through WordPress, instead of FTP:
+
+1. Upload the downloaded zip file on the 'Add New' plugins screen (see the 'Upload' tab) in your WordPress admin area and activate.
+
+== Screenshots ==
+
+User import screen
+
+== Changelog ==
+
+= 0.1 =
+* First release.
+
+== Upgrade Notice ==
+
+= 0.5 =
+First release.
